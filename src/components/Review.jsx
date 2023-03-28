ceac9333b9e155897e5114b529568b7ee94f9fda
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:44:31 +0100

    fix: modify

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -38,10 +38,10 @@ const Review = () => {
   };
 
   return (
-    <article className="review">
-      <div className="img-container">
-        <img src={image} alt={name} className="person-img" />
-        <span className="quote-icon">
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
           <FaQuoteRight />
         </span>
       </div>
