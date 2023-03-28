1d48b09678b5e162f8af9317f281162748c5cdc5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-28 01:57:30 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -22,7 +22,7 @@ const reviews = [
     name: 'peter jones',
     job: 'intern',
     image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     text:
       "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
